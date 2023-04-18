dfe1ca4bed31dd755a192d33592e307b83a8b520
WyeK <83228783+WyeK@users.noreply.github.com>
2023-04-18 20:27:31 +0800

    Add Flask

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
 def index():
     return render_template("index.html", )
+
+
+@app.route("/bus_navigation")
+def bus_navigation():
+    return render_template("bus_navigation.html", )
 
 
 if __name__ == "__main__":
